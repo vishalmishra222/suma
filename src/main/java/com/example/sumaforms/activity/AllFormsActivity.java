5b6c8a5f0efd93a2a76fb8430f7f6f19abe3e3f5
sumasoft <vishal.mishra@sumasoft.net>
2020-07-08 18:12:00 +0530

    dynamic form rendering library with gradle file

--- src/main/java/com/example/sumaforms/activity/AllFormsActivity.java
+++ src/main/java/com/example/sumaforms/activity/AllFormsActivity.java
@@ -259,142 +259,25 @@ public class AllFormsActivity extends AppCompatActivity implements AllFormsFragm
 
     }
 
-    /*private void addMenusToScollView(final List<SubCategory> subCategoryMenuList)
-    {
-        final String[] colors = getResources().getStringArray(R.array.horizontal_ntb);
-        imagesLinearLayout.removeAllViews();
-        imagesLinearLayout.invalidate();
-        for (int i = 0; i < subCategoryMenuList.size(); i++) {
-            try {
-                final ImageView imageView = new ImageView(getApplicationContext());
-                LinearLayout.LayoutParams relparams = new LinearLayout.LayoutParams(50, 50);
-                relparams.setMargins(10,5,5,10);
-                imageView.setLayoutParams(relparams);
-                if(subCategoryMenuList.size()>menuImages.length) {
-                    imageView.setBackgroundResource(R.drawable.ic_launcher);
-
-                }
-                else
-                {
-                    imageView.setBackgroundResource(menuImages[i]);
-                }
-                imageView.setTag("imageView".concat(""+i));
-                imageView.setId(i);
-
-                imagesLinearLayout.addView(imageView);
-
-                imageView.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        String tag = imageView.getTag().toString();
-                        tag = tag.replace("imageView", "");
-                        int position = Integer.parseInt(tag);
-                        mPager.setCurrentItem(position);
-                    }
-                });
-            }
-            catch (Exception e)
-            {
-
-            }
-        }
-
-    }*/
-
-   /* private void addMenusToScollView1(final List<SubCategory> subCategoryMenuList)
-    {
-        final String[] colors = getResources().getStringArray(R.array.horizontal_ntb);
-      //  imagesLinearLayout.removeAllViews();
-      //  imagesLinearLayout.invalidate();
-        for (int i = 0; i < subCategoryMenuList.size(); i++) {
-            try {
-                Menu menu = bottomNavigationView.getMenu();
-                menu.add(subCategoryMenuList.get(i).getSubcategory_name());
-            }
-            catch (Exception e)
-            {
-
-            }
-        }
-
-    }*/
-
-  /*private void setNavigationTabs(final List<SubCategory> subCategoryMenuList) {
-       final String[] colors = getResources().getStringArray(R.array.horizontal_ntb);
-       final ArrayList<NavigationTabBar.Model> models = new ArrayList<>();
-       for (int i = 0; i < subCategoryMenuList.size(); i++) {
-               try {
-                   models.add(
-                           new NavigationTabBar.Model.Builder(
-                                   getResources().getDrawable(menuImages[i]),
-                                   Color.parseColor(colors[i]))
-                                   .selectedIcon(getResources().getDrawable(menuImages[i]))
-                                   .build()
-                   );
-               } catch (Exception e) {
-                   models.add(
-                           new NavigationTabBar.Model.Builder(
-                                   getResources().getDrawable(R.drawable.ic_launcher),
-                                   getResources().getColor(R.color.accent))
-                                   .selectedIcon(getResources().getDrawable(R.drawable.ic_launcher))
-                                   .build()
-                   );
-               }
-       }
-
-
-       navigationTabBar.setModels(models);
-       navigationTabBar.setViewPager(mPager, cPosition);
-       navigationTabBar.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-           @Override
-           public void onPageScrolled(final int position, final float positionOffset, final int positionOffsetPixels) {
-
-           }
-
-           @Override
-           public void onPageSelected(final int position) {
-
-           }
-
-           @Override
-           public void onPageScrollStateChanged(final int state) {
-
-           }
-       });
-   }*/
-
     @Override
     public void onBackPressed() {
         IOUtils.showExitFormMessage(AllFormsActivity.this,getResources().getString(R.string.form_exit),AllFormsActivity.this);
-
-
     }
 
     public void initView() {
          toolbar = (Toolbar) findViewById(R.id.app_bar);
          tabLayout = (TabLayout) findViewById(R.id.tablayout);
-         //imageHorizontalScrollView = (HorizontalScrollView) findViewById(R.id.imagesHorizontalScrollView);
-         //imagesLinearLayout = (LinearLayout) findViewById(R.id.imagesLinearLayout);
-        // bottomNavigationView.setItemTextColor(getResources().getColor(R.color.accent));
          setSupportActionBar(toolbar);
          getSupportActionBar().setDisplayShowTitleEnabled(true);
          getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-
-
     }
-    private void showAToast(String message) {
 
+    private void showAToast(String message) {
         try {
             if (toast != null) {
                 toast.cancel();
             }
-           /* toast = Toast.makeText(mContext, message, Toast.LENGTH_SHORT);
-            TextView toastMessage = (TextView) toast.getView().findViewById(android.R.id.message);
-            toastMessage.setTextColor(Color.WHITE);
-            toastMessage.setBackgroundColor(Color.RED);
-            toast.show();*/
             LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             toastRoot = inflater.inflate(R.layout.my_toast, null);
             ImageView iv_left_icon = (ImageView) toastRoot.findViewById(R.id.iv_left_icon);
@@ -405,7 +288,6 @@ public class AllFormsActivity extends AppCompatActivity implements AllFormsFragm
             tv_msg.setText(message);
             tv_msg.setTextColor(Color.WHITE);
             tv_msg.setTextSize(16);
-
 //        parent_layout.setBackgroundDrawable(createToastBackground(context, parent_layout));
             parent_layout.setBackgroundResource(R.drawable.info_message_background);
 
@@ -416,10 +298,5 @@ public class AllFormsActivity extends AppCompatActivity implements AllFormsFragm
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-
     }
-
-
-
 }
