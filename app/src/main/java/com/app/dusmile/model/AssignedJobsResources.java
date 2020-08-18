f8d548188b70b5e43b4aff4d3af5cac5479502cc
sumasoft <vishal.mishra@sumasoft.net>
2020-08-18 20:36:21 +0530

    assigned jobs changes has been changed

---
+++ app/src/main/java/com/app/dusmile/model/AssignedJobsResources.java
@@ -0,0 +1,29 @@
+package com.app.dusmile.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class AssignedJobsResources {
+        @SerializedName("dataApi")
+        @Expose
+        private String dataApi;
+        @SerializedName("method")
+        @Expose
+        private String method;
+
+        public String getDataApi() {
+            return dataApi;
+        }
+
+        public void setDataApi(String dataApi) {
+            this.dataApi = dataApi;
+        }
+
+        public String getMethod() {
+            return method;
+        }
+
+        public void setMethod(String method) {
+            this.method = method;
+        }
+}
