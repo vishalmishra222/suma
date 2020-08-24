96eaa8e1e8899fcecc314a2d5e137cce75357e14
sumasoft <vishal.mishra@sumasoft.net>
2020-08-24 17:09:37 +0530

    const file changed

--- app/src/main/java/com/app/dusmile/preferences/Const.java
+++ app/src/main/java/com/app/dusmile/preferences/Const.java
@@ -18,66 +18,66 @@ public class Const {
     // public static final String BASE_URL = "http://192.168.222.54:8080/Dusmile/";//Ankita
    // public static final String BASE_URL = "http://192.168.43.242:8080/Dusmile/";
     //public static final String BASE_URL = "http://192.168.43.15:8080/Dusmile/";//samiksha local
-    public static final String BASE_URL = "http://demockross.sumasoft.com:9090/Dusmile";//MOHIT
+    public static final String BASE_URL = "http://demockross.sumasoft.com:9090/Dusmile/";//MOHIT
     //public static final String BASE_URL = "http://49.248.24.148/Dusmile/";//public
-     public static final String ALTERNATE_BASE_IP = "http://demockross.sumasoft.com/Dusmile";//mohit
+     public static final String ALTERNATE_BASE_IP = "http://demockross.sumasoft.com/Dusmile/";//mohit
    // public static final String BASE_URL = "http://192.168.111.226:8080/Dusmile/";//UAT
 
-    public String URL = BASE_URL;
+    public String URL = "http://demockross.sumasoft.com:9090/Dusmile";
 
-    public String REQUEST_LOGIN1 = BASE_URL + "/login";
+    public String REQUEST_LOGIN1 = BASE_URL + "login";
 
-    public String REQUEST_FIREBASE = BASE_URL + "/pushNotification/storeToken";
+    public String REQUEST_FIREBASE = BASE_URL + "pushNotification/storeToken";
+    
+    public String REQUEST_AVAILABLE_JOBS = BASE_URL + "getAvailableJobs";
 
-    public String REQUEST_AVAILABLE_JOBS = BASE_URL + "/getAvailableJobs";
+    public String GET_ASSIGNED_REPORT_METADATA = BASE_URL + "workflow/WEMI_FI/reportmetadata/JR4";
 
-    public String GET_ASSIGNED_REPORT_METADATA = BASE_URL + "/workflow/WEMI_FI/reportmetadata/JR4";
+    public String GET_COMPLETED_REPORT_METADATA = BASE_URL + "workflow/WEMI_FI/reportmetadata/JR6";
 
-    public String GET_COMPLETED_REPORT_METADATA = BASE_URL + "/workflow/WEMI_FI/reportmetadata/JR6";
+    public String GET_PENDING_WITH_BRANCH_REPORT_METADATA = BASE_URL + "workflow/WEMI_FI/reportmetadata/JR21";
 
-    public String GET_PENDING_WITH_BRANCH_REPORT_METADATA = BASE_URL + "/workflow/WEMI_FI/reportmetadata/JR21";
+    public String REQUEST_JOB_DETAILS = BASE_URL + "getJobDetails";
 
-    public String REQUEST_JOB_DETAILS = BASE_URL + "/getJobDetails";
+    public static final String DATABASE_NAME = "WEMI";
 
-    public static final String DATABASE_NAME = "/WEMI";
+    public String REQUEST_GET_UPDATED_TEMPLATE = BASE_URL + "ui/template/FI";
 
-    public String REQUEST_GET_UPDATED_TEMPLATE = BASE_URL + "/ui/template/FI";
+    public String REQUEST_DEASSIGN_JOB = BASE_URL + "unassignJob";
 
-    public String REQUEST_DEASSIGN_JOB = BASE_URL + "/unassignJob";
+    public String REQUEST_CONFIRM_TEMPLATE_UPDATE = BASE_URL + "confirmTemplate";
 
-    public String REQUEST_CONFIRM_TEMPLATE_UPDATE = BASE_URL + "/confirmTemplate";
+    public String REQUEST_CHANGE_PASS = BASE_URL + "user/updatePassword";
 
-    public String REQUEST_CHANGE_PASS = BASE_URL + "/user/updatePassword";
+    public String REQUEST_SAVE_SUBMIT_JOB_DETAILS1 = BASE_URL + "updateJob";
 
-    public String REQUEST_SAVE_SUBMIT_JOB_DETAILS1 = BASE_URL + "/updateJob";
+    public String REQUEST_SAVE_SUBMIT_JOB_DETAILS = BASE_URL + "jobs";
 
-    public String REQUEST_SAVE_SUBMIT_JOB_DETAILS = BASE_URL + "/jobs";
+    public String REQUEST_CHECK_ASSIGNED_JOBS = BASE_URL + "assignJobToContractor";
 
-    public String REQUEST_CHECK_ASSIGNED_JOBS = BASE_URL + "/assignJobToContractor";
+    public String REQUEST_UPLOAD_PDF = BASE_URL + "uploadFile" + "/" + DATABASE_NAME;
 
-    public String REQUEST_UPLOAD_PDF = BASE_URL + "/uploadFile" + "/" + DATABASE_NAME;
+    public String REQUEST_AUTO_UPLOAD_LOGS = BASE_URL + "uploadLogFile";
 
-    public String REQUEST_AUTO_UPLOAD_LOGS = BASE_URL + "/uploadLogFile";
+    public String REQUEST_GET_JOB_COUNT = BASE_URL + "jobs/assignedJobCount";
 
-    public String REQUEST_GET_JOB_COUNT = BASE_URL + "/jobs/assignedJobCount";
+    public String REQUEST_UPDATE_JOB_STATUS = BASE_URL + "job/updateStatus";
 
-    public String REQUEST_UPDATE_JOB_STATUS = BASE_URL + "/job/updateStatus";
-
-    public String REQUEST_GET_AVAILABLE_JOBS = BASE_URL + "/jobs/Available";
+    public String REQUEST_GET_AVAILABLE_JOBS = BASE_URL + "jobs/Available";
 
     //public String REQUEST_GET_ASSIGNED_JOBS = BASE_URL + "jobs/Assigned";
 
-    public String REQUEST_GET_ASSIGNED_JOBS = BASE_URL + "/reports/listProcess/JR4";
+    public String REQUEST_GET_ASSIGNED_JOBS = BASE_URL + "reports/listProcess/JR4";
 
-    public String REQUEST_GET_COMPLETED_JOBS = BASE_URL + "/reports/listProcess/JR6";
+    public String REQUEST_GET_COMPLETED_JOBS = BASE_URL + "reports/listProcess/JR6";
 
-    public String REQUEST_JOB_DETAILS1 = BASE_URL + "/job";
+    public String REQUEST_JOB_DETAILS1 = BASE_URL + "job";
 
-    public String REQUEST_REPORT_DETAILS = BASE_URL + "/reports/Status%20Report";
+    public String REQUEST_REPORT_DETAILS = BASE_URL + "reports/Status%20Report";
 
-    public String REQUEST_REPORT_FILTERS = BASE_URL + "/reports/listProcess/JR21";
+    public String REQUEST_REPORT_FILTERS = BASE_URL + "reports/listProcess/JR21";
 
-    public String REQUEST_HOLD_JOB = BASE_URL + "/jobs/Hold";
+    public String REQUEST_HOLD_JOB = BASE_URL + "jobs/Hold";
 
     public static String TAG = "DUSMILE";
 
@@ -97,7 +97,7 @@ public class Const {
     public static void setOneURL(Context _context, String oneURL) {
         mContext = _context;
         UserPreference.setBaseUrl(mContext, oneURL);
-        String BASE_URL = "http://" + getOneURL() + "/Dusmile";
+        String BASE_URL = "http://" + getOneURL() + "/Dusmile/";
     }
 
     public static Context getmContext() {
